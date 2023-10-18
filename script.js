909c0e0557e024dc2f09b473a4ddc8d57102265c
Timothy Williamson <atimiw@gmail.com>
2023-10-18 10:06:15 +0100

    Initial .json parsing setup in script.js, in prep for full portfolio item generation.

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+// Populate the portfolio projects data
+fetch("data.json").then(function (response) {
+    return response.json();
+}). then(function(data) {
 
-console.log("test");
+    //Debug check the data
+    console.log(data);
+
+    //Select where to display the data    
+    const displayTarget = document.querySelector("#portfolioDisplay");
+
+    //Empty string to build up with HTML + project data
+    let output = '';
+     
+
+     
+
+    //Update the display target
+    displayTarget.innerHTML += output;
+
+})

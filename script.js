ba52f3e76a82bfd5e3158df7142fe6e6d56d7426
Timothy Williamson <atimiw@gmail.com>
2023-10-19 16:24:08 +0100

    Updated the modal and close button formatting, alongside the colours palette.

--- script.js
+++ script.js
@@ -144,6 +144,15 @@ function popupClose(){
     modal.style.display = "none";
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementById("modalClose");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  console.log("clicked")
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
